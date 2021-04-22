13f24c15173ed8767e9490d7a7871f727e0719aa
Alejandro Daza <alejandro-daza@users.noreply.github.com>
2021-04-22 18:13:51 -0500

    Uplift integration working with test data

--- pueblo-bonito-checkout/new_js.js
+++ pueblo-bonito-checkout/new_js.js
@@ -1,3 +1,20 @@
+$("#upCheckbox").on('change',function(ev){
+   var checked = $(this).prop('checked');
+   if(checked){
+      $("#up-pay-monthly-container").show();
+      // $(".uplift-banner").show();
+      $(".cc_info").hide();
+   }else{
+      $("#up-pay-monthly-container").hide();
+      // $(".uplift-banner").hide();
+      $(".cc_info").show();
+   }
+   
+   
+});
+
+ // --------- UPLIFT INTEGRATION --------------------------
+
 window.upReady = function(){
    console.log('up Ready');
    window.Uplift.Agent.init(getInitConfig());
@@ -14,17 +31,9 @@ window.upReady = function(){
      const tripId = getTripId();
      window.Uplift.Agent.createTrip(tripInfo); 
    }
- }
+}
 
- function formatDate(date){
-   var newDate = new Date(date);
-   newDate = newDate.getFullYear().toString()
-             + newDate.getMonth().toString()
-             + newDate.getDate().toString();
-   return newDate;
- }
-
- function getInitConfig(){
+function getInitConfig(){
    const config = {
      agent:{
        id: $("#email2").val(),   //required
@@ -38,6 +47,22 @@ window.upReady = function(){
      currency: 'USD',
      onChange: function(tripOffer){
        console.log('On Change: '+tripOffer.status);
+       if(tripOffer.status == "OFFER_AVAILABLE"){
+         $(".upsell_div").show();
+         if(tripOffer.tripId != null){
+            console.log("saving id: "+tripOffer.tripId);
+            setCookie('tripId', tripOffer.tripId, 60);
+         }else{
+            console.log("No TripId");
+         }
+       }
+       if(tripOffer.status == "SERVICE_UNAVAILABLE"){
+         $(".upsell_div").hide();
+       }
+       if(tripOffer.status == "OFFER_UNAVAILABLE"){
+         console.log("saving id: "+ null);
+         setCookie('tripId', null, 60);
+       }
        return;
      },
      sessionTokenCallback: function(token){
@@ -47,26 +72,26 @@ window.upReady = function(){
      checkout: true
    }
    return config;
- }
+}
 
- function getTripInfo(){
+function getTripInfo(){
    var total = parseFloat($("#TotalDue").val()).toFixed(2);
    total = parseInt(total.replace('.',''));
    var travelers = [
      {
        id: 0,
-       first_name: $("#fname1").val(),
-       last_name: $("#lname1").val()
+       first_name: 'Arthur',//$("#fname1").val(),
+     last_name: 'Davis',//$("#lname1").val()
      },
-     {
-       id: 1,
-       first_name: $("#fname2").val(),
-       last_name: $("#fname2").val()
-     },
+   //   {
+   //     id: 1,
+   //     first_name: $("#fname2").val(),
+   //     last_name: $("#fname2").val()
+   //   },
    ];
    var billing_contact = {
-     first_name: $("#fname1").val(),
-     last_name: $("#lname1").val()
+     first_name: 'Arthur',//$("#fname1").val(),
+     last_name: 'Davis',//$("#lname1").val()
    };
 
    var arrival = formatDate($("#aDate").val());
@@ -86,8 +111,42 @@ window.upReady = function(){
      hotel_reservations: hotel_reservations
    };
    return tripInfo;
- }
+}
 
- function getTripId(){
+function getTripId(){
    return 12341234;
- }
+}
+
+// ---- UTILS ---------------
+
+function formatDate(date){
+   var newDate = new Date(date);
+   newDate = newDate.getFullYear().toString()
+             + newDate.getMonth().toString()
+             + newDate.getDate().toString();
+   return newDate;
+}
+
+ 
+function setCookie(cname, cvalue, exdays) {
+   var d = new Date();
+   d.setTime(d.getTime() + (exdays*24*60*60*1000));
+   var expires = "expires="+ d.toUTCString();
+   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+   var name = cname + "=";
+   var decodedCookie = decodeURIComponent(document.cookie);
+   var ca = decodedCookie.split(';');
+   for(var i = 0; i <ca.length; i++) {
+     var c = ca[i];
+     while (c.charAt(0) == ' ') {
+       c = c.substring(1);
+     }
+     if (c.indexOf(name) == 0) {
+       return c.substring(name.length, c.length);
+     }
+   }
+   return "";
+}
